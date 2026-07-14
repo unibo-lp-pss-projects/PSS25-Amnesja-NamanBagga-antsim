7fceee6a25bbcfd192c6efd39ff967aa5d1a4a70
amnesja <naman.bagga@studio.unibo.it>
2026-07-14 19:53:28 +0200

    feat(agent): implementazione dell'enum AntState che definisce i tre stati comportamentali della formica: - WANDERING (esplorazione/ricerca cibo); - TARGETING_FOOD(trovato cibo); - RETURNIN_TO_NEST (riportare il cibo verso il nido).
     Inoltre è stata implementata la classe Ant, ovvero l'agente principale della simulazione. Modella una formica con:
     - Coordinate Continue: WorldPosition con x e y;
     - Orientamento in radianti: angle;
     - Velocità fissa;
     - Stato comportamentale: AntState;
     - fìFlag per il trasporto di cibo;
    
    Il metodo principale è move(tick, world) che:
    - Calcola lo spostamente tramite la trigonometria con la seguente formula:
        - x: (velocità * cos(angolo) * durata del movimento);
        - y: (velocità * sin(angolo) * durata movimento);
    - Verifica collisioni con World.isBlockedAt(nextPos);
    - In caso di collisione, inverte la direzione con una deviazione casuale (angolo + pi + random(-0.5, 0.5)) simulando un rimbalzo.
    
    feat(test): implementazione del test AntMovementTest che verifica:
    - Movimento libero: una formica in una determinata posizione con un angolo 0 si sposta ad una determinata posizione dopo 1 tick;
    - Collisione con ostacolo: un formica posizionata di fronte a un ostacolo non lo attraversa e cambia angolo simulando il rimbalzo.

--- src/test/java/it/unibo/antsim/agent/AntMovementTest.java
+++ src/test/java/it/unibo/antsim/agent/AntMovementTest.java
@@ -1,5 +1,51 @@
 package it.unibo.antsim.agent;
 
-class AntMovementTest {
+import it.unibo.antsim.world.CellContent;
+import it.unibo.antsim.world.CellIndex;
+import it.unibo.antsim.world.World;
+import it.unibo.antsim.world.WorldPosition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class AntMovementTest {
+    private World world;
+    private static final double CELL_SIZE = 10.0;
+
+    @BeforeEach
+    void setUp(){
+        // Creating small world
+        world = new World(5, 5,CELL_SIZE, CELL_SIZE);
+    }
+
+    @Test
+    void TestContinuosMovementInFreeSpace(){
+        Ant ant = new Ant(new WorldPosition(25.0, 25.0), 0.0, 2.0);
+
+        // move 1 time tick
+        ant.move(1.0, world);
+
+        // Expected position after moving 1 tick
+        assertEquals(27.0, ant.getPosition().x(), 0.001);
+        assertEquals(25.0, ant.getPosition().y(), 0.001);
+    }
+
+    @Test
+    void testMovementCollisionWithWalls(){
+        // creating an obstacle
+        world.getGrid().setCellContent(new CellIndex(2, 3), new CellContent.Obstacle());
+
+        // positioning the ant right in front of the obstacle
+        Ant ant = new Ant(new WorldPosition(29.0, 25.0), 0.0, 2.0);
+
+        ant.move(1.0, world);
+
+        // The ant should not have moved into the obstacle
+        assertNotEquals(31.0, ant.getPosition().x());
+        assertTrue(ant.getPosition().x() <= 30.0, "The ant walked on the obstacle!!!");
+
+        // The ant should have bounced back, so its angle should have changed
+        assertNotEquals(0.0, ant.getAngle(), "The ant did not bounce");
+    }
 }
